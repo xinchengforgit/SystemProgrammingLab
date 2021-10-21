5217bfd397c7332dea30a23d4657fffac8438310
xinchengforgit <2419743144@qq.com>
2021-10-21 19:34:26 +0800

    complete some notes and the first function in week1 lab

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -4,10 +4,16 @@ use std::collections::HashSet;
 
 fn main() {
     println!("Hi! Try running \"cargo test\" to run tests.");
+    let mut v: Vec<i32> = Vec::new(); //注意这个mut
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    //
+    let mut ret = Vec::new();
+    for i in v.iter() {
+        ret.push(i + n);
+    }
+    ret
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
